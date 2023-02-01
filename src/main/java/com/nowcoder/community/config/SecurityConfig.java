18bdbe264e9ebd7da938759c263b8b82dc92ab22
orangejun <di277160461@qq.com>
2023-02-01 16:16:53 +0800

    Initial commit

--- src/main/java/com/nowcoder/community/config/SecurityConfig.java
+++ src/main/java/com/nowcoder/community/config/SecurityConfig.java
@@ -129,7 +129,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter implements Comm
                 )
                 .antMatchers(
                         "/discuss/delete",
-                        "/data/**"
+                        "/data/**",
+                        "/actuator/**"
                 )
                 .hasAnyAuthority(
                         AUTHORITY_ADMIN

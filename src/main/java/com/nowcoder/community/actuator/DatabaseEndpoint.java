804252c82be79c0866bc648b8281f8c4a275d03c
orangejun <di277160461@qq.com>
2023-02-01 16:17:56 +0800

    Initial commit

---
+++ src/main/java/com/nowcoder/community/actuator/DatabaseEndpoint.java
@@ -0,0 +1,44 @@
+package com.nowcoder.community.actuator;
+
+import com.nowcoder.community.util.CommunityUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.actuate.endpoint.annotation.Endpoint;
+import org.springframework.boot.actuate.endpoint.annotation.ReadOperation;
+import org.springframework.stereotype.Component;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+/**
+ * @program: community
+ * @description:
+ * @author: DM
+ * @create: 2023-02-01 14:25
+ **/
+// 不属于任何一层 是一个公共组件
+@Component
+@Endpoint(id = "database")
+public class DatabaseEndpoint {
+
+    private static final Logger logger = LoggerFactory.getLogger(DatabaseEndpoint.class);
+
+    @Autowired
+    private DataSource dataSource;
+
+    //该注解表示该请求为get请求
+    @ReadOperation
+    public String checkConnection() {
+        try (
+                Connection conn = dataSource.getConnection();
+        ) {
+            return CommunityUtil.getJSONString(0, "获取连接成功!");
+        } catch (SQLException e) {
+            logger.error("获取连接失败:" + e.getMessage());
+            return CommunityUtil.getJSONString(1, "获取连接失败!");
+        }
+    }
+
+}
